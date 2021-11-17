85c74d980ea0d6649cc957e3187e2078638fc15f
Anas Ansari <anasansari157@gmail.com>
2021-11-17 16:54:03 +0530

    Creates Tabs for notes.

---
+++ app/javascript/src/components/Dashboard/Notes/Tabs.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { Search, Plus, Settings } from "@bigbinary/neeto-icons";
+import { MenuBar } from "@bigbinary/neetoui/v2/layouts";
+import { Typography } from "neetoui/v2";
+
+export default function Tabs({ setIsSearchCollapsed, isSearchCollapsed }) {
+  return (
+    <>
+      <MenuBar showMenu={true} title="Notes">
+        <MenuBar.Block label="All" count={200} />
+        <MenuBar.Block label="Users" count={80} />
+        <MenuBar.Block label="Leads" count={60} />
+        <MenuBar.Block label="Visitors" count={60} />
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: () => <Search size={20} />,
+              onClick: () => setIsSearchCollapsed(!isSearchCollapsed),
+            },
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Segments
+          </Typography>
+        </MenuBar.SubTitle>
+
+        <MenuBar.Search
+          collapse={isSearchCollapsed}
+          onCollapse={() => setIsSearchCollapsed(true)}
+        />
+
+        <MenuBar.Block label="Europe" count={80} />
+        <MenuBar.Block label="Middle-East" count={60} />
+        <MenuBar.Block label="Asia" count={60} />
+
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: () => <Settings size={20} />,
+            },
+            {
+              icon: () => <Plus size={20} />,
+            },
+            {
+              icon: () => <Search size={20} />,
+            },
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Tags
+          </Typography>
+        </MenuBar.SubTitle>
+
+        <MenuBar.Block label="Sales" count={80} />
+        <MenuBar.Block label="Finance" count={60} />
+        <MenuBar.Block label="User Experience" count={60} />
+      </MenuBar>
+    </>
+  );
+}
